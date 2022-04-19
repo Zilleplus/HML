037fed9d3366c14e58fa1abe1d22561fadd2684f
zilleplus <zillepus@gmail.com>
2022-04-19 23:01:28 +0200

    Ex9pytorch of chapter14 now works
    * Still a lot slower to converge then the tensorflow version. Not sure
      why.

--- HML/chapter14/ex9pytorch.py
+++ HML/chapter14/ex9pytorch.py
@@ -1,15 +1,20 @@
 import torch
-import torchvision
+import torchvision.datasets as ds
 import torchvision.transforms as transforms
 from torch import nn
 
-mnist_train_data = torchvision.datasets.MNIST(download=True, root='MNIST/processed/training.pt',
-                                              transform=transforms.ToTensor())
-mnist_test_data = torchvision.datasets.MNIST(download=True, root='MNIST/processed/test.pt')
+mnist_train_data = ds.MNIST(download=True,
+                            root='MNIST/processed/training.pt',
+                            transform=transforms.ToTensor())
+mnist_test_data = ds.MNIST(download=True,
+                           root='MNIST/processed/test.pt',
+                           transform=transforms.ToTensor())
 
 batch_size = 16
-train_data_loader = torch.utils.data.DataLoader(dataset=mnist_train_data, batch_size=batch_size)
-test_data_loader = torch.utils.data.DataLoader(dataset=mnist_test_data, batch_size=batch_size)
+train_data_loader = torch.utils.data.DataLoader(dataset=mnist_train_data,
+                                                batch_size=batch_size)
+test_data_loader = torch.utils.data.DataLoader(dataset=mnist_test_data,
+                                               batch_size=batch_size)
 
 # Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -19,21 +24,24 @@ print(f"Using {device} device")
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.conv_layer1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, padding="same")
-        self.max_pool = nn.MaxPool2d(kernel_size=2)
-        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding="same")
-        self.conv_layer3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, padding="same")
+        self.conv_layer1 = nn.Conv2d(in_channels=1, out_channels=32,
+                                     kernel_size=7, padding="same")
+        self.max_pool = nn.MaxPool2d(kernel_size=2).to(device)
+        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=64,
+                                     kernel_size=3, padding="same")
+        self.max_pool = nn.MaxPool2d(kernel_size=2).to(device)
+        self.conv_layer3 = nn.Conv2d(in_channels=64, out_channels=128,
+                                     kernel_size=1, padding="same")
 
         self.flatten = nn.Flatten()
 
         self.output_layer1 = nn.Sequential(
-            nn.Linear(in_features=128*7*7, out_features=128),
+            nn.Linear(in_features=128 * 3 * 3, out_features=128),
             nn.ReLU())
 
         self.output_layer2 = nn.Sequential(
             nn.Linear(in_features=128, out_features=10),
-            nn.Softmax()
-        )
+            nn.Softmax())
 
     def forward(self, x):
         x = self.conv_layer1(x)
@@ -41,6 +49,7 @@ class NeuralNetwork(nn.Module):
         y = self.conv_layer2(x)
         x = self.max_pool(y)  # drop down to 7*7
         x = self.conv_layer3(x)
+        x = self.max_pool(x)  # drop down 3*3 -> it rounds down
 
         x = self.flatten(x)
         x = self.output_layer1(x)
@@ -49,7 +58,7 @@ class NeuralNetwork(nn.Module):
         return x
 
 
-model = NeuralNetwork()
+model = NeuralNetwork().to(device)
 print(model)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
@@ -81,20 +90,20 @@ def test(dataloader, model, loss_fn):
     model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
-        for X, y in enumerate(dataloader):
+        for batch, (X, y) in enumerate(dataloader):
             X, y = X.to(device), y.to(device)
 
             pred = model(X)
 
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-            break
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%\
+          , Avg loss: {test_loss:>8f} \n")
 
 
-epochs = 5
+epochs = 100
 for t in range(epochs):
     print(f"Epoch {t + 1}\n-------------------------------")
     train(train_data_loader, model, loss_fn, optimizer)
